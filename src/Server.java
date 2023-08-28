c72506cc134d9ccc714d31b9d5d3b3365c7b21c1
emil51z8 <emil51z8@edu.zealand.dk>
2023-08-28 14:04:57 +0200

    Opgaven løst

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,37 @@
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    static int port = 2001;
+    static DataInputStream in;
+    static DataOutputStream out;
+    static ServerSocket server;
+    static Socket socket;
+
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(port);
+            socket = server.accept();
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            try {
+
+                String modtaget = in.readUTF();
+                System.out.println("Besked modtaget: " + modtaget);
+
+                out.writeUTF("Denne besked er modtaget: " + modtaget);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
